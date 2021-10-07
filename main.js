2745d9b1c61df63643e6a33c8dfcde48eb1c834a
Alex Vitkov <alexvitkov98@gmail.com>
2021-10-07 17:44:55 +0300

    Removed canvas resize

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 console.log(PIXI)
 
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+
 const app = new PIXI.Application({
-    width: 800,
-    height: 600,
-    resizeTo: window,
+    width: CANVAS_WIDTH,
+    height: CANVAS_HEIGHT,
 });
 document.body.appendChild(app.view);
 
 
 // Load the background
 const background = PIXI.Sprite.from("assets/bg.jpg");
+background.width = CANVAS_WIDTH;
+background.height = CANVAS_HEIGHT;
 app.stage.addChild(background);
 
-function onResize(newWidth, newHeight) {
-    background.width = newWidth;
-    background.height = newHeight;
-}
-app.renderer.resize = onResize;
-onResize(app.view.width, app.view.height);
 
-init();
+async function loadRockets() {
+    const resp = await fetch("https://api.spacexdata.com/v2/rockets");
+    const data = await resp.json();
+
+    console.log(data);
+}
+
+loadRockets();
+
 
 // const rocketTexture = PIXI.Texture.from("assets/rocket.png");
 // const rocketSprite = new PIXI.Sprite(rocketTexture);
