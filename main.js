ff3ae43b98cd72fa0fafcedfa0d04c98e2d9bb2c
Alex Vitkov <alexvitkov98@gmail.com>
2021-10-08 09:59:52 +0300

    Rockets losing bottom part after first_stage

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const ROCKET_SCALE = 0.015;
 const ROCKET_SPEED = 2;
 const BOTTOM_Y_OFFSET = 30;
 
+const BOTTOM_PART_GRAVITY_SCALE = 0.14;
+const BOTTOM_PART_ROTATION_SCALE = 0.001;
+const BOTTOM_PART_MAX_ROTATION_SPEED = 0.1;
+const BOTTOM_PART_SCALE_DECREASE_SCALE = 0.003;
+
 const app = new PIXI.Application({
     width: CANVAS_WIDTH,
     height: CANVAS_HEIGHT,
@@ -28,6 +33,14 @@ let time = 0.0;
 
 function random(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+function clamp(val, min, max) {
+    if (val < min)
+        return min;
+    if (val > max)
+        return max;
+    return val;
 }
 
 class FuelBar {
@@ -57,6 +70,43 @@ class FuelBar {
     }
 }
 
+class FloatingBottomPart {
+    constructor(sprite, initialVelocity) {
+        this.sprite = sprite;
+        this.velocity = initialVelocity;
+        this.angularVelocity = 0;
+        this.xAcceleration = random(-.1, .1);
+
+        const pos = sprite.getGlobalPosition();
+        sprite.parent.removeChild(sprite);
+
+        app.stage.addChild(sprite);
+        sprite.x = pos.x;
+        sprite.y = pos.y;
+
+        this.tickFn = this.tick.bind(this);
+        app.ticker.add(this.tickFn);
+    }
+
+    tick() {
+        const dt = app.ticker.deltaTime;
+        this.velocity += BOTTOM_PART_GRAVITY_SCALE * dt;
+        this.angularVelocity += BOTTOM_PART_ROTATION_SCALE * dt;
+        if (this.angularVelocity > BOTTOM_PART_MAX_ROTATION_SPEED)
+            this.angularVelocity = BOTTOM_PART_MAX_ROTATION_SPEED;
+            
+
+        this.sprite.y += BOTTOM_PART_GRAVITY_SCALE * this.velocity;
+        this.sprite.rotation += this.angularVelocity * dt;
+        this.sprite.scale.set(this.sprite.scale.x - BOTTOM_PART_SCALE_DECREASE_SCALE * dt);
+
+        if (this.sprite.scale.x < 0) {
+            app.ticker.remove(this.tickFn);
+            this.sprite.destroy();
+        }
+    }
+}
+
 class Rocket {
     constructor(x, rocketData) {
         this.initialFuel1 = this.fuel1 = rocketData.first_stage.fuel_amount_tons;
@@ -66,16 +116,16 @@ class Rocket {
 
         this.container = new PIXI.Container();
 
-
-
         // The rocket sprite itself
         this.rocketSprite = new PIXI.Sprite(rocketTopTexture);
         this.rocketBottomSprite = new PIXI.Sprite(rocketBottomTexture);
         this.rocketSprite.anchor.set(0.5, 1.5);
-        this.rocketBottomSprite.anchor.set(0.5, 1);
+        this.rocketBottomSprite.anchor.set(0.5, .5);
 
         this.rocketScale = rocketData.height.meters * ROCKET_SCALE;
         this.rocketSize = this.rocketScale * (rocketTopTexture.height + rocketBottomTexture.height);
+
+        this.rocketBottomSprite.y = -rocketBottomTexture.height * this.rocketScale * 0.5;
 
         this.rocketSprite.scale.set(this.rocketScale)
         this.rocketBottomSprite.scale.set(this.rocketScale);
@@ -110,22 +160,36 @@ class Rocket {
         app.ticker.add(this.tick.bind(this));
     }
 
+    lose_bottom_part() {
+        new FloatingBottomPart(this.rocketBottomSprite, clamp(this.yVelocity, -10, 0));
+    }
+
     get_fuel_percentage() {
         return (this.fuel1 + this.fuel2) / (this.initialFuel1 + this.initialFuel2);
     }
 
 
     update_y() {
-        let percentage = this.get_fuel_percentage();
-        let y = (CANVAS_HEIGHT - this.rocketSize) * percentage + this.rocketSize;
-        this.container.y = y;
+        let p = this.get_fuel_percentage();
+
+        p = 1 - (1 - p) * (1 - p); // square interpolation looks nicer
+
+        let new_y = (CANVAS_HEIGHT - this.rocketSize) * p + this.rocketSize;
+        let old_y = this.container.y;
+        this.container.y = new_y;
+
+        // this is needed for when we lose the bottom part,
+        // to give it a sensible initial velocity
+        this.yVelocity = (new_y - old_y) / app.ticker.deltaTime;
     }
 
     tick() {
         if (this.fuel1 > 0) {
             this.fuel1 -= app.ticker.deltaTime;
-            if (this.fuel1 < 0)
+            if (this.fuel1 <= 0) {
                 this.fuel1 = 0;
+                this.lose_bottom_part();
+            }
         }
         else {
             this.fuel2 -= app.ticker.deltaTime;
