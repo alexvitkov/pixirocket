e9d0c317df0189a141652208b6b52c0c5448a6f7
Alex Vitkov <alexvitkov98@gmail.com>
2021-10-08 12:51:58 +0300

    Removed unused settings from Config.js

--- src/Config.js
+++ src/Config.js
@@ -2,9 +2,9 @@ export const SCREEN_WIDTH = 800;
 export const SCREEN_HEIGHT = 600;
 
 export const ROCKET_SCALE = 0.015;
-export const ROCKET_SPEED = 2;
-export const BOTTOM_Y_OFFSET = 30;
 
+// The following control how the animation of the bottom part looks
+// after it gets detached from the rocket
 export const BOTTOM_PART_GRAVITY_SCALE = 0.14;
 export const BOTTOM_PART_ROTATION_SCALE = 0.001;
 export const BOTTOM_PART_MAX_ROTATION_SPEED = 0.1;
