df92ab073817b52e297db30fad85afe01275022d
Alex Vitkov <alexvitkov98@gmail.com>
2021-10-08 12:55:48 +0300

    Moved ScreenText config variables to Config.js

--- src/Config.js
+++ src/Config.js
@@ -9,3 +9,10 @@ export const BOTTOM_PART_GRAVITY_SCALE = 0.14;
 export const BOTTOM_PART_ROTATION_SCALE = 0.001;
 export const BOTTOM_PART_MAX_ROTATION_SPEED = 0.1;
 export const BOTTOM_PART_SCALE_DECREASE_SCALE = 0.003;
+
+// The following control the labels that appear in the middle of the screen
+export const ScreenText = {
+    INITIAL_Y_SPEED: 1.5,
+    Y_SPEED_CHANGE: -0.015,
+    ALPHA_DECREASE: 0.007,
+};
--- src/ScreenText.js
+++ src/ScreenText.js
@@ -5,7 +5,7 @@ import * as Config from "./Config";
 function animateText(text) {
 
     let scaleAdd = 0;
-    let yAdd = 1.5;
+    let yAdd = Config.ScreenText.INITIAL_Y_SPEED;
 
     const tickerFn = () => {
         const dt = App.ticker.deltaTime;
@@ -14,24 +14,19 @@ function animateText(text) {
         const scale = text.scale.x + scaleAdd * dt;
         text.scale.set(scale);
 
-        yAdd -= 0.015 * dt;
+        yAdd += Config.ScreenText.Y_SPEED_CHANGE * dt;
         if (yAdd < 0)
             yAdd = 0;
         text.y -= yAdd * dt;
 
-        text.alpha -= 0.007 * dt;
+        text.alpha -= Config.ScreenText.ALPHA_DECREASE * dt;
         if (text.alpha <= 0) {
             App.ticker.remove(tickerFn);
             text.destroy();
         }
-
-        //if (scale <= 0) {
-        //}
     }
 
     App.ticker.add(tickerFn);
-
-
 }
 
 export function write(text) {
